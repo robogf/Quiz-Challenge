333093259c49285b1e847828dd19e7ec6abebc96
robogf <111620315+robogf@users.noreply.github.com>
2022-10-03 17:30:54 -0700

    added the display for our question on our start button

--- script.js
+++ script.js
@@ -1,27 +1,27 @@
 var startButton = document.getElementById('start-btn')
 var mainContainer = document.getElementById ('main-container')
 var timeCon = document.getElementById ('timeId')
+var questionContainer = document.getElementById ('question-container')
 var timer = 75
 
 startButton.addEventListener('click',startQuiz)
-
+document.getElementById('question-container').style.display = "none";
 function startQuiz(){
     
     mainContainer.style.display = "none";
+    questionContainer.style.display = "block";
     function setTime() {
 
         timeInterval = setInterval(function(){
             timer-- 
             if (timer <1){
                 clearInterval(timeInterval)
-                
             }
             timeCon.innerHTML=timer;
         },1000)
        
     }
     setTime();
-    
     
 
 
