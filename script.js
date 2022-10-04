28a7a79fa8fcbab2791e45c9530612114dc4ec3b
robogf <111620315+robogf@users.noreply.github.com>
2022-10-03 19:51:58 -0700

    added the questions to our quiz

--- script.js
+++ script.js
@@ -2,14 +2,73 @@ var startButton = document.getElementById('start-btn')
 var mainContainer = document.getElementById ('main-container')
 var timeCon = document.getElementById ('timeId')
 var questionContainer = document.getElementById ('question-container')
+var question = document.getElementById('question')
+var answerChoices= document.getElementById ('answer-buttons')
+var answerSelect= document.getElementsByClassName(".ansBtn")
 var timer = 75
+var questions = [
+    {
+        question: 'Which built-in method adds one or more elements to the end of an array and returns the new length of the array?',
+        answers: [
+          {text: 'last()',  correct: false},
+          {text: 'put()', correct: false},
+          {text: 'push()', correct: true},
+          {text: 'None of the Above', correct:false}
+        ]
+    },
+    {
+        question: 'Which built-in method reverses the order of the elements of an array?',
+        answers: [
+            {text: 'changeOrder(order)', correct:false},
+            {text: 'reverse()', correct:true},
+            {text: 'sort(order)', correct:false},
+            {text: 'None of the above ', correct:false},
+
+            
+        ]
+    },
+    {
+        question: 'Which of the following function of String object returns the characters in a string between two indexes into the string?',
+        answers: [
+            {text: ' slice()', correct:false},
+            {text: ' split()', correct:false},
+            {text: ' substr()', correct:false},
+            {text: ' substring()', correct:true}
+        ]
+    },
+    {
+        question:'Which of the following function of String object creates an HTML anchor that is used as a hypertext target?',
+        answers: [
+            {text: ' anchor()', correct:true},
+            {text: ' link ()', correct:false},
+            {text: ' blink())', correct:false},
+            {text: ' big()', correct:false},
+
+
+        ]
+    },
+    {
+        question: 'Which of the following function of Array object calls a function for each element in the array?',
+        answers: [
+            {text: ' concat()', correct:false},
+            {text: ' every()', correct:false},
+            {text: ' filter()', correct:false},
+            {text: ' forEach()', correct:true},
+        ]
+    }
+]
+
 
 startButton.addEventListener('click',startQuiz)
+answerChoices.addEventListener('click',)
 document.getElementById('question-container').style.display = "none";
+
+
 function startQuiz(){
     
     mainContainer.style.display = "none";
     questionContainer.style.display = "block";
+
     function setTime() {
 
         timeInterval = setInterval(function(){
@@ -22,11 +81,5 @@ function startQuiz(){
        
     }
     setTime();
-    
-
-
 }
-
-
-
 
